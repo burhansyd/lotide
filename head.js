59c632740939710f84b49c0888174e602024b9d0
Burhan Syed <burhan.syed@outlook.com>
2021-07-19 23:45:34 +0000

    heads were identified

---
+++ head.js
@@ -0,0 +1,18 @@
+function head (arr) {
+  if (arr.length >= 1) {
+    return arr[0];
+  } else {
+    return undefined;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F911) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(String.fromCodePoint(0x1F923) + ` Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
