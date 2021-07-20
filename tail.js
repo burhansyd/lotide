669d183a7687b74296b28e3be45abdaa747955f2
Burhan Syed <burhan.syed@outlook.com>
2021-07-20 00:46:39 +0000

    modifications

--- tail.js
+++ tail.js
@@ -29,4 +29,7 @@ const tail = function(arr) {
 
 
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
