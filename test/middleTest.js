16f105f92ee302002a9b2e173323fde045235950
Burhan Syed <burhan.syed@outlook.com>
2021-07-26 19:58:15 +0000

    refactored with required modules

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require('../assertArraysEqual.js');
+const middle = require('../middle.js')
+
+console.log(assertArraysEqual(middle([1]), [])); // => []
+console.log(assertArraysEqual(middle([1, 2]), [])); // => []
+console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // => [2]
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])); // => [3]
+console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])); // => [2, 3]
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])); // => [3, 4]
