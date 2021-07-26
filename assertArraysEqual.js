b253a2218ae73d2f35668153cbb6bc89db7606b9
Burhan Syed <burhan.syed@outlook.com>
2021-07-26 19:57:29 +0000

    changed arr.length to true in if statement

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === arr1.length) {
+  if (eqArrays(arr1, arr2) === true) {
     console.log(String.fromCodePoint(0x1F911) + ` Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(String.fromCodePoint(0x1F923) + ` Assertion failed: ${arr1} !== ${arr2}`);
