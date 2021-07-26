d9d3b34a12b72b25d744b02bbd806f004e35c34c
Burhan Syed <burhan.syed@outlook.com>
2021-07-26 19:02:22 +0000

    refactored with module exports

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(String.fromCodePoint(0x1F911) + ` Assertion Passed: ${actual} === ${expected}`);
+    return String.fromCodePoint(0x1F911) + ` Assertion Passed: ${actual} === ${expected}`;
   } else {
-    console.log(String.fromCodePoint(0x1F923) + ` Assertion failed: ${actual} !== ${expected}`);
+    return String.fromCodePoint(0x1F923) + ` Assertion failed: ${actual} !== ${expected}`;
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
+
--- head.js
+++ head.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   if (arr.length >= 1) {
     return arr[0];
@@ -6,13 +8,4 @@ const head = function(arr) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(0x1F911) + ` Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(0x1F923) + ` Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
