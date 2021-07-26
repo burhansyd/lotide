26db574378f11bca03a75eb1a291bfdc0d041b2e
Burhan Syed <burhan.syed@outlook.com>
2021-07-26 19:44:04 +0000

    refactored to require eqArrays and module export function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let count = 0;
-  for (let ele1 of arr1) {
-    for (let ele2 of arr2) {
-      if (arr1.length === arr2.length && ele1 === ele2 && arr1.indexOf(ele1) === arr2.indexOf(ele2)) {
-        return count += 1;
-      }
-    }
-  }
-};
-
+const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === arr1.length) {
@@ -17,3 +7,5 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(String.fromCodePoint(0x1F923) + ` Assertion failed: ${arr1} !== ${arr2}`);
   }
 };
+
+module.exports = assertArraysEqual;
