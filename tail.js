b0f35d800a5ec35d1db44da52926e6a89684fddb
Burhan Syed <burhan.syed@outlook.com>
2021-07-26 19:31:21 +0000

    refactored with module export

--- tail.js
+++ tail.js
@@ -1,24 +1,7 @@
-const assertEqual = function(actual, expected) {
-  let count = 0;
-  if (actual.length === expected.length) {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] === expected[i]) {
-        count += 1;
-      }
-    }
-    if (count === actual.length) {
-      console.log(String.fromCodePoint(0x1F911) + ` Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(String.fromCodePoint(0x1F923) + ` Assertion failed: ${actual} !== ${expected}`);
-    }
-  } else {
-    console.log(String.fromCodePoint(0x1F923) + ` Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-let newArr = [];
+const assertEqual = require('./assertEqual.js')
 
 const tail = function(arr) {
+  let newArr = [];
   for (let i = 0; i < arr.length; i++) {
     if (i !== 0) {
       newArr.push(arr[i]);
@@ -27,9 +10,4 @@ const tail = function(arr) {
   return newArr;
 };
 
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+module.exports = tail;
